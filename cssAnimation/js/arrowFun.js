a7c89a113ffe2c2fc8862a8c71a14ef76501607a
rakesh <rakeshrock750@gmail.com>
2024-05-29 10:24:43 +0530

    addd arow

--- cssAnimation/js/arrowFun.js
+++ cssAnimation/js/arrowFun.js
@@ -1,9 +1,9 @@
 const user = {
-    username : "Steve",
-    price : 999,
+    username: "Steve",
+    price: 999,
 
-    welcomeMessage : function ( ) {
-        console.log( `${this.username} , welcome to website ` ); // current conext
+    welcomeMessage: function () {
+        console.log(`${this.username} , welcome to website `); // current conext
         console.log(this);
     }
 }
@@ -15,7 +15,7 @@ const user = {
 // console.log(this); // you get empty object
 
 function chai() {
-    
+
     console.log(this);
 }
 
