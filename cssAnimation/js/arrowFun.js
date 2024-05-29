27a2b29fe9d75823e2c2b95f3710e4ba78803d79
rakesh <rakeshrock750@gmail.com>
2024-05-29 11:57:57 +0530

    new arrowfun line

--- cssAnimation/js/arrowFun.js
+++ cssAnimation/js/arrowFun.js
@@ -1,12 +1,12 @@
-const user = {
-    username: "Steve",
-    price: 999,
+// const user = {
+//     username: "Steve",
+//     price: 999,
 
-    welcomeMessage: function () {
-        console.log(`${this.username} , welcome to website `); // current conext
-        console.log(this);
-    }
-}
+//     welcomeMessage: function () {
+//         console.log(`${this.username} , welcome to website `); // current conext
+//         console.log(this);
+//     }
+// }
 
 // user.welcomeMessage()
 // user.username = "sam"
@@ -44,9 +44,51 @@ const user = {
 
 //const addTwo = (num1 , num2) => (num1 + num2)  // no need to write return keyword
 
-const addTwo = (num1 , num2) => ({username : " neymar"})
+// const addTwo = (num1 , num2) => ({username : " neymar"})
 // console.log(addTwo(3 , 4));
 
 // const myArray = [2,3,5,4,8,9]
 
 // myArray.forEach()
+
+//=================(IIfe) Immediately Invoke function Expressions ===============
+
+// (function chai() { its is named IIfe
+//     console.log(`DB CONNECT`);
+// })();
+
+( (name) => {
+    console.log(`DB Connected TWO ${name}`);
+} )('Ramos');
+
+// ================Call Stacks ===============================
+
+/**
+ * javascript Context
+ * Global EC (exection context)
+ * 
+ * it exectute under thread js is signel thereaded
+ * Function EC / Functioal EC
+ * Eval EC
+ * 
+ * it run in tWo fase ---------
+ * 
+ * ===> memory creation phase
+ * ===> Exection phase
+ * 
+ * class Stack
+ */
+
+let val1 = 10 
+let val2 = 5
+
+function addNum(num1 , num2) {
+    let total = num1 + num2
+    return total
+}
+
+let result1 = addNum(val1,val2)
+let result2 = addNum(10,2)
+
+//============== control flow =================
+
